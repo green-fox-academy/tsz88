dfbcbd9c20918613bf97a36a005c477cb361839b
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-10 14:23:37 +0200

    EnvelopeStar works perfectly :-)

--- week-02/day-10/src/EnvelopeStar.java
+++ week-02/day-10/src/EnvelopeStar.java
@@ -7,37 +7,47 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class EnvelopeStar {
 
   public static void mainDraw(Graphics graphics){
-    int step = 40;
+    int step = 20;
     //create array that holds all the center point in the middle, vertical axis
-    int[][] verticalCenterPoints = new int[HEIGHT/step][2];
-    for (int i = 0; i < HEIGHT/step; i++){
+    int[][] verticalCenterPoints = new int[HEIGHT/step+1][2];
+    for (int i = 0; i <= HEIGHT/step; i++){
       verticalCenterPoints[i][0] = WIDTH/2;
       verticalCenterPoints[i][1] = i*step;
     }
 
-    //print all vertical points
-
-
     //create array that holds all the center point in the middle, horizontal axis
-    int[][] horizontalCenterPoints = new int[WIDTH/step][2];
-    for (int k = 0; k < WIDTH/step; k++){
+    int[][] horizontalCenterPoints = new int[WIDTH/step+1][2];
+    for (int k = 0; k <= WIDTH/step; k++){
       horizontalCenterPoints[k][0] = k*step;
       horizontalCenterPoints[k][1] = HEIGHT/2;
     }
-    for (int l = 0; l < horizontalCenterPoints.length; l++){
-      for (int m = 0; m < 2; m++) {
-        System.out.print(horizontalCenterPoints[l][m]);
-      }
-      System.out.println();
-    }
-    //I know that the HEIGHT constant is larger than WIDTH
+//    for (int l = 0; l < horizontalCenterPoints.length; l++){
+//      for (int m = 0; m < 2; m++) {
+//        System.out.print(horizontalCenterPoints[l][m]);
+//      }
+//      System.out.println();
+//    }
+    graphics.setColor(Color.BLACK);
+    int m = WIDTH/step/2; //x value of the central point on the horizontal axis
 
-    for (int v = 0; v < HEIGHT/step; v++){
-      for (int h = 0; h < WIDTH/step; h++){
-        graphics.drawLine(verticalCenterPoints[v][0],verticalCenterPoints[v][1],horizontalCenterPoints[h][0],horizontalCenterPoints[h][1]);
-      }
+    System.out.println(verticalCenterPoints.length+ " & "+horizontalCenterPoints.length);
+    //connect the vertical axis with every horizontal dot on the left side
+    for (int v = 0; v <= HEIGHT/step; v++){
+        if (v <= m){
+          graphics.drawLine(verticalCenterPoints[v][0],verticalCenterPoints[v][1],horizontalCenterPoints[v+m][0],horizontalCenterPoints[v+m][1]);
+          graphics.drawLine(verticalCenterPoints[v][0],verticalCenterPoints[v][1],horizontalCenterPoints[m-v][0],horizontalCenterPoints[m-v][1]);
+       }
+       if (v > m){
+         graphics.drawLine(verticalCenterPoints[v][0],verticalCenterPoints[v][1],horizontalCenterPoints[v-m][0],horizontalCenterPoints[v-m][1]);
+         graphics.drawLine(verticalCenterPoints[v][0],verticalCenterPoints[v][1],horizontalCenterPoints[WIDTH/step-v+m][0],horizontalCenterPoints[WIDTH/step-v+m][1]);
+       }
     }
-  }
+    }
+
+
+
+
+
 
   //    Don't touch the code below
   static int WIDTH = 400;
