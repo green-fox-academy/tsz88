cec278f173f741c203d2c04aba7ad1370feadbe6
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-14 11:39:46 +0200

    PrintEachLine first version added - not tested which actual input file

---
+++ week-03/day-12/src/PrintEachLine.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PrintEachLine {
+  public static void main(String[] args){
+
+    Path myFilePath = Paths.get(my-file.txt);
+    List<String> lines = null;
+
+    try {
+      lines = Files.readAllLines(myFilePath);
+    } catch (IOException e){
+      System.out.println("Unable to read file: my-file.txt");
+    }
+  }
+}
