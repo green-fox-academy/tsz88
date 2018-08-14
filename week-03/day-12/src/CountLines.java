000643a7f33b29861977e834c80c3bb3ad899546
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-14 11:55:40 +0200

    countlines java first version added, exceptions not finished

---
+++ week-03/day-12/src/CountLines.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class CountLines {
+  public static void main(String[] args){
+    System.out.println("Tell me the filename, please!");
+    Scanner scanner = new Scanner(System.in);
+    String filename = scanner.nextLine();
+    Path path = Paths.get(filename);
+
+    try{
+      int fileLinesNumber = Files.readAllLines(path).size();
+    } catch (IOException x){
+      System.out.println(0);
+    }
+
+  }
+}
