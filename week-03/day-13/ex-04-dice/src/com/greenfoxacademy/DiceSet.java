8a60a3c1fe7ab328a3880f090c08d3e8d52700f6
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-15 15:44:41 +0200

    dice java added and works

---
+++ week-03/day-13/ex-04-dice/src/com/greenfoxacademy/DiceSet.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+public class DiceSet {
+
+  // You have a `DiceSet` class which has a list for 6 dices
+// You can roll all of them with roll()
+// Check the current rolled numbers with getCurrent()
+// You can reroll with reroll()
+// Your task is to roll the dices until all of the dices are 6
+  int[] dices = new int[6];
+
+  public int[] roll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+    return dices;
+  }
+
+  public int[] getCurrent() {
+    return dices;
+  }
+
+  public int getCurrent(int i) {
+    return dices[i];
+  }
+
+  public void reroll() {
+    for (int i = 0; i < dices.length; i++) {
+      dices[i] = (int) (Math.random() * 6) + 1;
+    }
+  }
+
+  public void reroll(int k) {
+    dices[k] = (int) (Math.random() * 6) + 1;
+  }
+
+  public static void main(String[] args) {
+    DiceSet diceSet = new DiceSet();
+    System.out.println(Arrays.toString(diceSet.getCurrent()));
+
+    diceSet.roll();
+
+    System.out.println(Arrays.toString(diceSet.getCurrent()));
+
+    int k = 0;
+
+    for (int i = 0; i < 6;) {
+      if (diceSet.getCurrent(i) == 6) {
+        i++;
+      } else {
+        diceSet.reroll(i);
+        System.out.println(Arrays.toString(diceSet.getCurrent()));
+      }
+    }
+  }
+}
