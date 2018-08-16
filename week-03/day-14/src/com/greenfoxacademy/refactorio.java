6dec00a7cd9fe684601701fadefaa85a3d081f09
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-16 13:39:25 +0200

    refactorio added, too easy because same task was in homework video

---
+++ week-03/day-14/src/com/greenfoxacademy/refactorio.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy;
+
+public class refactorio {
+  public static void main(String[] args){
+    System.out.println(factorial(5));
+  }
+  
+  public static int factorial(int x){
+    if (x == 1){
+      return 1;
+    } else {
+      return factorial(x-1)*x;
+    }
+  }
+}
