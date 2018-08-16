830319db86dee96a91f84caff09a77016529fcb9
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-16 13:40:02 +0200

    refactorio name fixed

--- week-03/day-14/src/com/greenfoxacademy/Refactorio.java
+++ week-03/day-14/src/com/greenfoxacademy/Refactorio.java
@@ -1,10 +1,10 @@
 package com.greenfoxacademy;
 
-public class refactorio {
+public class Refactorio {
   public static void main(String[] args){
     System.out.println(factorial(5));
   }
-  
+
   public static int factorial(int x){
     if (x == 1){
       return 1;
