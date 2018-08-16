2afa6f249e12976d0c8225be842e5badaa9bd471
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-16 12:33:31 +0200

    bunniesagain works, with sort of cheating with double method

--- week-03/day-14/src/com/greenfoxacademy/BunniesAgain.java
+++ week-03/day-14/src/com/greenfoxacademy/BunniesAgain.java
@@ -1,16 +1,20 @@
-//package com.greenfoxacademy;
-//
-//public class BunniesAgain {
-//  public static void main(String[] args){
-//    System.out.println(mutantBunnyEarCounter(2));
-//  }
-//
-//
-////  public static int mutantBunnyEarCounter (int bunny){
-//    if (bunny == 0){
-//      return 0;
-//    } else {
-//
-//    }
-//  }
-//}
+package com.greenfoxacademy;
+
+public class BunniesAgain {
+  public static void main(String[] args){
+    System.out.println(mutantBunnyEarCounterInt(12));
+  }
+
+
+  public static double mutantBunnyEarCounterDouble (int bunny){
+    if (bunny == 0){
+      return 0;
+    } else {
+      return mutantBunnyEarCounterDouble(bunny-1) + 2.5;
+    }
+  }
+
+  public static int mutantBunnyEarCounterInt (int bunny){
+    return (int)(mutantBunnyEarCounterDouble(bunny));
+  }
+}
