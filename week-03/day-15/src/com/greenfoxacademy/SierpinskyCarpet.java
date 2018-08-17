5211e6ab3f60bfa82aca70a84f52fc1f9f294d32
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-17 13:10:12 +0200

    sierpinsky carpet started, first column fixed

---
+++ week-03/day-15/src/com/greenfoxacademy/SierpinskyCarpet.java
@@ -0,0 +1,46 @@
+package com.greenfoxacademy;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class SierpinskyCarpet {
+  public static void mainDraw(Graphics graphics){
+    fractalPattern(300, 310,310,graphics);
+  }
+  public static void fractalPattern(int size, int x, int y, Graphics graphics){
+    if(size < 1){
+    } else {
+      graphics.fillRect(x, y, size,size);
+      //left side column
+      fractalPattern(size/3,x-size*2/3, y-size*2/3, graphics);
+      fractalPattern(size/3,x-size*2/3, y+size*1/3, graphics);
+      fractalPattern(size/3,x-size*2/3, y+size*4/3, graphics);
+
+
+    }
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
