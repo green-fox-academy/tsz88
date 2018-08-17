a0766e281dfcdb05eb5e873b1df4e367a68420e1
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-17 11:23:05 +0200

    hexagonfractal - basic unit draws fine

--- week-03/day-15/src/com/greenfoxacademy/HexagonFractal.java
+++ week-03/day-15/src/com/greenfoxacademy/HexagonFractal.java
@@ -8,9 +8,19 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class HexagonFractal {
   public static void mainDraw(Graphics graphics){
+    basicUnit(450,300,50,graphics);
 
 
 
+  }
+
+  public static void basicUnit(int side, int x, int y, Graphics graphics){
+    //first point is the top left edge of the hexagon
+    int height = (int)(Math.sqrt(3)/2*side);
+    int[] xPoints = {x, x + side, x + side * 3 / 2, x + side, x, x - side / 2};
+    int[] yPoints = {y, y, y + height, y + height * 2, y + height * 2, y + height};
+
+    graphics.drawPolygon(xPoints,yPoints,6);
   }
 
   // Don't touch the code below
