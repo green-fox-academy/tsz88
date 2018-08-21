30b91ea9961b3fa4a57f58096d4ce2d8e85972df
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-21 16:40:49 +0200

    added all features to aircraftcarrier, has no syntax error, but not yet tested with example data

--- week-04/AircraftCarrier/src/com/greenfoxacademy/Aircraft.java
+++ week-04/AircraftCarrier/src/com/greenfoxacademy/Aircraft.java
@@ -2,44 +2,43 @@ package com.greenfoxacademy;
 
 public class Aircraft {
   protected String type;
-  protected int ammo;
+  public int ammo;
   protected int baseDamage;
   public int maxAmmo;
 
 
-  public Aircraft(){
+  public Aircraft() {
     ammo = 0;
   }
 
-  public int fight(){
+  public int fight() {
     int damage = ammo * baseDamage;
     ammo = 0;
     return damage;
   }
 
-  public int refill(int availableAmmo){
-    if (maxAmmo-ammo > availableAmmo){
+  public int refill(int availableAmmo) {
+    if (maxAmmo - ammo > availableAmmo) {
       ammo += availableAmmo;
       availableAmmo = 0;
     } else {
       availableAmmo = availableAmmo - (maxAmmo - ammo);
       ammo = maxAmmo;
-    } return availableAmmo;
+    }
+    return availableAmmo;
   }
 
-  public String getType(){
+  public String getType() {
     return type;
   }
-  public void getstatus(){
-    System.out.println("Type: " + type + ",\nAmmo: " + ammo + ",\nBase damage: " + baseDamage + "\n All damage: " + ammo * baseDamage);
-  }
-  public boolean isPrority(){
-    if (type.equals("F35")){
-      return true;
-    } else {
-      return false;
-    }
+
+  public void getstatus() {
+    System.out.println("Type: " + type + ", Ammo: " + ammo + ", Base damage: " + baseDamage + " All damage: " + ammo * baseDamage);
   }
 
-
+  public boolean isPrority() {
+    return type.equals("F35");
+  }
 }
+
+
--- week-04/AircraftCarrier/src/com/greenfoxacademy/Carrier.java
+++ week-04/AircraftCarrier/src/com/greenfoxacademy/Carrier.java
@@ -7,29 +7,64 @@ public class Carrier {
   int carrierAmmo;
   int healthPoints;
 
-  public Carrier(int initialCarrierAmmo, int healthPoints){
+  public Carrier(int initialCarrierAmmo, int healthPoints) {
     this.carrierAmmo = initialCarrierAmmo;
     this.healthPoints = healthPoints;
   }
 
-  public void add(Aircraft x){
+  public void add(Aircraft x) {
     carrier.add(x);
   }
 
-  public void fill(){
-    
-    if (carrierAmmo <= 0){
+  public void fill() {
+    int totalFillable = 0;
+    for (int i = 0; i < carrier.size(); i++) {
+      totalFillable += carrier.get(i).maxAmmo - carrier.get(i).ammo;
+    }
+    if (carrierAmmo <= 0) { //no ammo exception
       System.out.println("No ammo to fill.");
-    } else if (){
-      for(int i = 0; carrierAmmo > 0 && i < carrier.size(); i++){
-        if(carrier.get(i).isPrority()){
-          carrierAmmo = carrier.get(i).refill(carrierAmmo);
-          i++;
-        }
+    }
+    if (totalFillable < carrierAmmo) { //enough ammo to fill all aircrafts
+      for (int j = 0; j < carrier.size(); j++) {
+        carrierAmmo = carrier.get(j).refill(carrierAmmo);
       }
     } else {
+      //start by filling the priority planes
+      for (int k = 0; k < carrier.size() && carrierAmmo > 0; k++)
+        if (carrier.get(k).isPrority()) {
+          carrierAmmo = carrier.get(k).refill(carrierAmmo);
+        } else {
+        }
+      //fill up no-priority planes as well until it runs out of ammo
+      int l = 0;
+      while (carrierAmmo > 0) {
+        carrierAmmo = carrier.get(l).refill(carrierAmmo);
+        l++;
+      }
+    }
+  }
 
+  public void fight(Carrier B) {
+    int totalFirePower = 0;
+    for (int i = 0; i < carrier.size(); i++) {
+      totalFirePower += carrier.get(i).fight();
+    }
+    B.healthPoints -= totalFirePower;
+  }
+
+  public void status() {
+    int totalDamage = 0;
+    for (int i = 0; i < carrier.size(); i++) {
+      totalDamage += carrier.get(i).maxAmmo * carrier.get(i).baseDamage;
     }
 
+    if (healthPoints <= 0) {
+      System.out.println("It's dead, Jim.:-(");
+    } else {
+      System.out.println("HP: " + healthPoints + ", Aircraft count: " + carrier.size() + ", AmmoStorage: " + carrierAmmo + ", Total damage: " + totalDamage + ".\n Aircrafts:");
+      for (int k = 0; k < carrier.size(); k++) {
+        carrier.get(k).getstatus();
+      }
+    }
   }
 }
