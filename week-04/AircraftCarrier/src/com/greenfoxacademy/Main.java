cc4db23098eb57a778e42335b3a4b7dcd4edc6db
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-21 16:48:17 +0200

    aircraftcarrier - tested with few sample data, should work okay

--- week-04/AircraftCarrier/src/com/greenfoxacademy/Main.java
+++ week-04/AircraftCarrier/src/com/greenfoxacademy/Main.java
@@ -2,7 +2,18 @@ package com.greenfoxacademy;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    Carrier awesomeness = new Carrier(2300, 5000);
 
-    }
+    awesomeness.add(new F16());
+    awesomeness.add(new F16());
+    awesomeness.add(new F35());
+    awesomeness.add(new F35());
+
+    awesomeness.status();
+    System.out.println();
+
+    awesomeness.fill();
+    awesomeness.status();
+  }
 }
