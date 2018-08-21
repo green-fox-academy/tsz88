53fb05efcde57943c74caaea7e9e936b3c7d5a85
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-21 11:34:26 +0200

    Added greenfoxinheritance java, started creating Person class

---
+++ week-04/greenfoxinheritence/src/com/greenfoxacademy/Person.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy;
+
+public class Person {
+
+  String name;
+  int age;
+  String gender;
+
+  //methods
+  public void introduce(){
+    System.out.println("Hi, I'm " + name + ", a " + age "-year-old " + gender + ".");
+  }
+
+}
