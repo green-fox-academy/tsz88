ec05e9f3d704d7039edd5b48fba05ab15e78bc01
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-22 16:30:49 +0200

    started instruments exercise, created main classes

---
+++ week-04/Instruments/src/main/java/music/Instrument.java
@@ -0,0 +1,8 @@
+package main.java.music;
+
+abstract public class Instrument {
+  protected String name;
+
+  public void play(){
+  }
+}
---
+++ week-04/Instruments/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,13 @@
+package main.java.music;
+
+public class StringedInstrument extends Instrument{
+  protected int numberOfStrings;
+
+
+  public void sound(){
+  }
+
+  public void play(){
+    sound();
+  }
+}
