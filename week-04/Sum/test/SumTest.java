d961ac8d107fb9dd0d2d431a4b5ad4a90424d1e0
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-23 15:10:18 +0200

    for Sum java tester case added where it works it with empty starting arraylist too

--- week-04/Sum/test/SumTest.java
+++ week-04/Sum/test/SumTest.java
@@ -16,4 +16,13 @@ public void sumMakerTest(){
   ArrayList<Integer> tester = new ArrayList<>(Arrays.asList(3,6,9,12));
   Assert.assertEquals(30, x.sumMaker(tester));
 }
+
+@Test
+public void emptyStarterForSum(){
+  ArrayList<Integer> q = new ArrayList<>();
+  Assert.assertEquals(0,x.sumMaker(q));
+}
+
+
+
 }
