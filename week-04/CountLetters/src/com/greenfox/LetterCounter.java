c995ee3816bb69a0c7c6a136d9c1cebb836dc50c
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-23 16:16:11 +0200

    LetterCounter added, main constructor added

---
+++ week-04/CountLetters/src/com/greenfox/LetterCounter.java
@@ -0,0 +1,16 @@
+package com.greenfox;
+
+import java.util.HashMap;
+
+public class LetterCounter {
+  private String x;
+
+  public LetterCounter(String x){
+    this.x = x;
+  }
+
+  public HashMap countThem(){
+    HashMap letters = new HashMap();
+    
+  }
+}
---
+++ week-04/CountLetters/test/LetterCounterTest.java
@@ -0,0 +1,7 @@
+import com.greenfox.LetterCounter;
+
+public class LetterCounterTest {
+
+  LetterCounter element = new LetterCounter();
+
+}
