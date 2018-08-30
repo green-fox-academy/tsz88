a2df265446c9e5059ffb685be519e4c527db571f
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-30 11:38:41 +0200

    mostproductiveyear finished, now it recognises multiple peak years and increments the hashmap counter in statistics map well

--- week-05/mostproductiveyear/src/com/greenfox/Main.java
+++ week-05/mostproductiveyear/src/com/greenfox/Main.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -36,17 +33,36 @@ public class Main {
     //statistics lists the specific year, and how many movies were made in it
     HashMap<Integer,Integer> statistics = new HashMap();
     for (int j = 0; j < eachEntryInFile.size(); j++){
-      statistics.put(Integer.valueOf(eachEntryInFile.get(j)[1]),statistics.get(Integer.valueOf(eachEntryInFile.get(j)[1]) + 1));
+      if (!statistics.containsKey(Integer.valueOf(eachEntryInFile.get(j)[1]))){
+        statistics.put(Integer.valueOf(eachEntryInFile.get(j)[1]), 1);
+      }
+      else {
+        statistics.replace(Integer.valueOf(eachEntryInFile.get(j)[1]), (statistics.get(Integer.valueOf(eachEntryInFile.get(j)[1])) + 1));
+      }
     }
 
     //search for the year with highest number of movies made in
     int mostMoviesNumber = 0;
-    for (int k = 0; k < statistics.size(); k++){
-      if (mostMoviesNumber < statistics.get(k)){
-        mostMoviesNumber = statistics.get(k)
+    ArrayList<Integer> succesfulYears = new ArrayList<>();
+    for (Map.Entry<Integer,Integer> entry : statistics.entrySet()){
+      if (entry.getValue() > mostMoviesNumber){
+        mostMoviesNumber = entry.getValue();
       }
     }
 
+    //collect out all the years that had that many movies made
+    for (Map.Entry<Integer,Integer> entry : statistics.entrySet()){
+      if (entry.getValue() == mostMoviesNumber){
+        succesfulYears.add(entry.getKey());
+      }
+    }
 
+    //make a pretty string of the successful years
+    String output = Integer.toString(succesfulYears.get(0));
+    for (int g = 1; g < succesfulYears.size(); g++){
+      output += ", " + Integer.toString(succesfulYears.get(g));
+    }
+
+    System.out.println(name + " has made the most movies in " + output);
   }
 }
