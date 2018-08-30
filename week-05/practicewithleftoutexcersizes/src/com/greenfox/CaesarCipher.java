c5cc9a35c58ccb72ab65fd153a8587a311048bed
Szilvia Toth <tothszilvia88@gmail.com>
2018-08-30 15:54:46 +0200

    finally managed to circumvent the uppercase.lowercase issue with the code by adding a second alphabet, does not solve the issue of going around if the shift exceeds the letter A or last signs of the alphabet

--- week-05/practicewithleftoutexcersizes/src/com/greenfox/CaesarCipher.java
+++ week-05/practicewithleftoutexcersizes/src/com/greenfox/CaesarCipher.java
@@ -10,39 +10,48 @@ import java.util.List;
 
 public class CaesarCipher {
 
-  public static void encryptTheCode(){
+  public static void encryptTheCode() {
     Path path = Paths.get("caesar.txt");
     List<String> lines = null;
 
     try {
       lines = Files.readAllLines(path);
-    } catch (IOException x){
+    } catch (IOException x) {
     }
 
     List<Character> alphabet = new ArrayList<>();
-    alphabet = (Arrays.asList('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','[',']', ',','-', '.', '/', '*', '+'));
+    alphabet = (Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '[', ']', ',', '-', '.', '/', '*', '+', '\'', '(', ')', '!', '"'));
+
+    List<Character> alphabet2 = new ArrayList<>();
+    alphabet2 = (Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', ',', '-', '.', '/', '*', '+','\'', '(', ')', '!', '"'));
 
     int shift = -1;
     List<String> decodedMessage = new ArrayList<>();
     int index = 0;
     String currentLine = "";
 
-    for (int row = 0; row < lines.size(); row++){
-      for (int letter = 0; letter < lines.get(row).length(); letter++){
+    for (int row = 0; row < lines.size(); row++) {
+      for (int letter = 0; letter < lines.get(row).length(); letter++) {
 
-        if (lines.get(row).charAt(letter) == ' '){
+        if (lines.get(row).charAt(letter) == ' ') {
           currentLine += " ";
-        } else try{
-        index = alphabet.indexOf(lines.get(row).charAt(letter));
-        currentLine += (String.valueOf(alphabet.get(index + shift)));
-      } catch (ArrayIndexOutOfBoundsException x){
+        } else if (alphabet.contains(lines.get(row).charAt(letter))) try {
+          index = alphabet.indexOf(lines.get(row).charAt(letter));
+          currentLine += (String.valueOf(alphabet.get(index + shift)));
+        } catch (ArrayIndexOutOfBoundsException x) {
         }
+        else try {
+            index = alphabet2.indexOf(lines.get(row).charAt(letter));
+            currentLine += (String.valueOf(alphabet2.get(index + shift)));
+          } catch (ArrayIndexOutOfBoundsException x) {
+          }
       }
-      decodedMessage.add(row,currentLine);
+      decodedMessage.add(row, currentLine);
       currentLine = "";
     }
 
-    System.out.println(decodedMessage);
-
+    for (int i = 0; i < decodedMessage.size(); i++) {
+      System.out.println(decodedMessage.get(i));
+    }
   }
 }
