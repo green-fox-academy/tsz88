5af4d9c00db7fa2ac54be4f56175c1d3624607f4
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-10 14:28:09 +0200

    exercise 6 added, works with capital letter names

--- week-06/day-01/streamExercises2/src/com/greenfox/Main.java
+++ week-06/day-01/streamExercises2/src/com/greenfox/Main.java
@@ -1,14 +1,27 @@
 package com.greenfox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
-    // write your code here
     String text = "AztAMinDenIT";
     System.out.println(countUpperCaseLetters(text));
+
+    ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
+    System.out.println(startsWithAEndsWithI(cities));
   }
+
+  private static ArrayList<String> startsWithAEndsWithI(ArrayList<String> cities) {
+    //this works with capital letter city-names only
+    return (ArrayList<String>) cities.stream()
+            .filter(city -> city.charAt(0) == 'A')
+            .filter(city -> city.charAt(city.length()-1) == 'I')
+            .collect(Collectors.toList());
+  }
+
 
   //exercise5
   //Write a Stream Expression to find the uppercase characters in a string!
