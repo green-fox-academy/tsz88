ac9f3f3625ff6fc190781d9dd7cae4984e46a85b
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-10 14:21:49 +0200

    exercise 5 made prettier in formatting

--- week-06/day-01/streamExercises2/src/com/greenfox/Main.java
+++ week-06/day-01/streamExercises2/src/com/greenfox/Main.java
@@ -1,24 +1,21 @@
 package com.greenfox;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-      String text = "AztAMinDenIT";
-      System.out.println(countUpperCaseLetters(text));
-    }
+  public static void main(String[] args) {
+    // write your code here
+    String text = "AztAMinDenIT";
+    System.out.println(countUpperCaseLetters(text));
+  }
 
   //exercise5
   //Write a Stream Expression to find the uppercase characters in a string!
   private static int countUpperCaseLetters(String text) {
     return text.chars()
         .mapToObj(letter -> (char) letter)
-        .filter(x->Character.isUpperCase(x))
+        .filter(x -> Character.isUpperCase(x))
         .collect(Collectors.toList()).size();
   }
-
-
 }
