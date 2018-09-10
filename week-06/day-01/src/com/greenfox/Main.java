c340ff2f116dfdfb0f001c2ae160a9ac2c30d06d
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-10 13:46:44 +0200

    exercise 4 finished

--- week-06/day-01/src/com/greenfox/Main.java
+++ week-06/day-01/src/com/greenfox/Main.java
@@ -6,37 +6,46 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-      ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+  public static void main(String[] args) {
+    // write your code here
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
 
 
-      System.out.println(evenNumbers(numbers));
-      System.out.println(oddNumbersAverage(numbers));
-      System.out.println(squareOfAllPositives(numbers));
+    System.out.println(evenNumbers(numbers));
+    System.out.println(oddNumbersAverage(numbers));
+    System.out.println(squareOfAllPositives(numbers));
+    System.out.println(squareIsLargerThan20(numbers2));
 
-    }
-//exercise3
+  }
+  //exercise4
+  private static ArrayList<Integer> squareIsLargerThan20(ArrayList<Integer> numbers2) {
+    return (ArrayList<Integer>) numbers2.stream()
+        .filter(number -> (number * number) > 20)
+        .collect(Collectors.toList());
+  }
+
+  //exercise3
   private static ArrayList<Integer> squareOfAllPositives(ArrayList<Integer> numbers) {
-      return (ArrayList<Integer>) numbers.stream()
-              .filter(number -> number > 0)
-              .map(number -> number * number)
-              .collect(Collectors.toList());
+    return (ArrayList<Integer>) numbers.stream()
+        .filter(number -> number > 0)
+        .map(number -> number * number)
+        .collect(Collectors.toList());
   }
 
   //exercise2
   private static int oddNumbersAverage(ArrayList<Integer> numbers2) {
-      return numbers2.stream()
-              .filter(number -> number % 2 != 0)
-              .reduce((number1, number2) -> number1 + number2)
-              .get();
+    return numbers2.stream()
+        .filter(number -> number % 2 != 0)
+        .reduce((number1, number2) -> number1 + number2)
+        .get();
   }
 
   //exercise1
   private static ArrayList<Integer> evenNumbers(ArrayList<Integer> numbers) {
-      return (ArrayList<Integer>) numbers.stream()
-                .filter(number -> number % 2 == 0)
-                .collect(Collectors.toList());
+    return (ArrayList<Integer>) numbers.stream()
+        .filter(number -> number % 2 == 0)
+        .collect(Collectors.toList());
   }
 
 
