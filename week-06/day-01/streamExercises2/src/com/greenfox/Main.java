77dd7d04d47b258a04d79aff9eae53b4acf36258
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-10 16:27:30 +0200

    exercise 8 added

--- week-06/day-01/streamExercises2/src/com/greenfox/Main.java
+++ week-06/day-01/streamExercises2/src/com/greenfox/Main.java
@@ -10,20 +10,29 @@ import java.util.stream.Collectors;
 public class Main {
 
   public static void main(String[] args) {
-    String text = "AztAMinDenIjjjjT";
+    String text = "NemSemmi";
     System.out.println(countUpperCaseLetters(text));
 
     ArrayList<String> cities = new ArrayList<>(Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"));
     System.out.println(startsWithAEndsWithI(cities));
 
     System.out.println(frequencyOfLetters(text));
+
+    ArrayList<Integer> numbers8 = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
+    System.out.println(frequencyOfNumbers(numbers8));
+  }
+
+  //exercise 8 number arraylist, what is the frequency of each number
+  private static Map<Integer, Long> frequencyOfNumbers(ArrayList<Integer> numbers8) {
+    return numbers8.stream()
+        .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
   }
 
   // exercise 7 unique letters with frequency in given text
   private static Map<Character, Long> frequencyOfLetters(String text) {
     Map<Character, Long> countedLetters =
         text.chars()
-            .mapToObj(c -> (char)c)
+            .mapToObj(c -> (char) c)
             .collect(Collectors.groupingBy(
                 c -> c, Collectors.counting()));
     return countedLetters;
