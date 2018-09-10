78c2ffb0c365f4f88c573fe3ca05cb632984d37c
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-10 17:20:34 +0200

    three ways added, how exercise 9 does not work

--- week-06/day-01/streamExercises3/src/com/greenfox/Main.java
+++ week-06/day-01/streamExercises3/src/com/greenfox/Main.java
@@ -3,25 +3,37 @@ package com.greenfox;
 import java.util.Arrays;
 import java.util.StringJoiner;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
-	//exercise 9
-      // Write a Stream Expression to convert a char array to a string!
+  public static void main(String[] args) {
+    //exercise 9
+    // Write a Stream Expression to convert a char array to a string!
 
-      char[] charArray = {'a','b','c','d'};
-
-      System.out.println(charArrayToString(charArray));
-    }
+    char[] charArray = {'a', 'b', 'c', 'd'};
+    System.out.println(charArrayToString(charArray));
+  }
 
   private static String charArrayToString(char[] charArray) {
-    return Arrays.asList(charArray).stream()
-        .collect(Collector.of(
-            () -> new StringJoiner(""),
-            (joiner,c) -> joiner.add(c.toString()),
-            (c1, c2) -> c1.merge(c2),
-            StringJoiner::toString
-        ));
+//    Collector<char[], StringJoiner, String> merger =
+//        Collector.of(
+//            () -> new StringJoiner(""),                 //supplier
+//            (joiner,c) -> joiner.add(c.toString()),     //accumulator
+//            (c1, c2) -> c1.merge(c2),                   //combiner
+//            StringJoiner::toString);                    //finisher
+//    String output = Arrays.asList(charArray)
+//        .stream()
+//        .collect(merger);
+//    return output;
+
+//    return Arrays.asList(charArray).stream()
+//        .map(c -> c.toString())
+//        .collect(Collectors.joining(""));
+
+      return Arrays.asList(charArray).stream()
+          .map(c -> (char)c)
+          .flatMap(Arrays::stream)
+          .collect(Collectors.joining(""));
   }
 }
