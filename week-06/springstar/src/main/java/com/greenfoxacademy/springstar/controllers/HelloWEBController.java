ddb56548c562392c9f7410b59c0b08e0c63b9bb8
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-17 15:04:46 +0200

    counter added to web hellow rold version, and also takes the name from requestparam

--- week-06/springstar/src/main/java/com/greenfoxacademy/springstar/controllers/HelloWEBController.java
+++ week-06/springstar/src/main/java/com/greenfoxacademy/springstar/controllers/HelloWEBController.java
@@ -3,12 +3,20 @@ package com.greenfoxacademy.springstar.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
 @Controller
 public class HelloWEBController {
 
+  AtomicLong loadedTheSiteThisManyTimes = new AtomicLong();
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(@RequestParam("name")String name, Model model) {
+    model.addAttribute("name", name);
+    model.addAttribute("counter",loadedTheSiteThisManyTimes.incrementAndGet());
     return "greeting";
   }
 }
