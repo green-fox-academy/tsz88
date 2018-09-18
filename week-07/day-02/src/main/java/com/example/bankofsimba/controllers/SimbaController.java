97a347d1de99e1c1a7ed039c4aae5c654bf718cc
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-18 17:29:29 +0200

    multiaccount html added, which lists animals with id generated from arraylist index

--- week-07/day-02/src/main/java/com/example/bankofsimba/controllers/SimbaController.java
+++ week-07/day-02/src/main/java/com/example/bankofsimba/controllers/SimbaController.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 
 public class SimbaController {
@@ -19,5 +23,16 @@ public class SimbaController {
   @RequestMapping("/htmlception")
   public String inception() {
     return "htmlception";
+  }
+
+  @RequestMapping("/multipleAccounts")
+  public String multi(Model model){
+    List<BankAccount> charactersAccountsList = new ArrayList<>(Arrays.asList(
+        new BankAccount("Simba", 2000, "lion"),
+        new BankAccount("Nala", 1000,"lion"),
+        new BankAccount("Shenzi", 200, "hyena"),
+        new BankAccount("Zazu", 5000, "red-billed hornbill")));
+    model.addAttribute("list",charactersAccountsList);
+    return "multiAccount";
   }
 }
