676fddcab8d1ec3e939dfb72803f2aa739437334
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-18 15:16:28 +0200

    created thymeleaf template for bankaccount

---
+++ week-07/day-02/src/main/java/com/example/bankofsimba/controllers/SimbaController.java
@@ -0,0 +1,18 @@
+package com.example.bankofsimba.controllers;
+
+import com.example.bankofsimba.model.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class SimbaController {
+
+  @RequestMapping("/show")
+  public String showBankAccount(Model model){
+    BankAccount simbas = new BankAccount("Simba",2000, "lion" );
+    model.addAttribute("BankAccount", simbas);
+    return "showbankaccount";
+  }
+}
---
+++ week-07/day-02/src/main/java/com/example/bankofsimba/model/BankAccount.java
@@ -0,0 +1,14 @@
+package com.example.bankofsimba.model;
+
+public class BankAccount {
+
+  String name;
+  int balance;
+  String animalType;
+
+  public BankAccount(String name, int balance, String animalType){
+    this.name = name;
+    this.balance = balance;
+    this.animalType = animalType;
+  }
+}
