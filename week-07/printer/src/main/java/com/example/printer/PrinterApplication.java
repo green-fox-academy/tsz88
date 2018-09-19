0da6fec422336004799616ded24178307aa6c1ee
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-19 14:45:38 +0200

    added controller for web task, created utility service class

--- week-07/printer/src/main/java/com/example/printer/PrinterApplication.java
+++ week-07/printer/src/main/java/com/example/printer/PrinterApplication.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class PrinterApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(PrinterApplication.class, args);
+    @RequestMapping("/useful")
   }
 
   @Override
   public void run(String... args) throws Exception {
     printer.log("hello");
     newColor.printColor();
-
   }
+
+
+
 
   @Autowired
   private Printer printer;
