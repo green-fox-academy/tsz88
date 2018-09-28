93affe7a74381ad6473858af9940103320b8dbb8
Szilvia Toth <tothszilvia88@gmail.com>
2018-09-28 14:02:36 +0200

    mysql todo app now creates database and drops it each time, basic user admin set up

--- week-08/todomysql/src/main/java/com/greenfoxacademy/todomysql/TodomysqlApplication.java
+++ week-08/todomysql/src/main/java/com/greenfoxacademy/todomysql/TodomysqlApplication.java
@@ -1,12 +1,25 @@
 package com.greenfoxacademy.todomysql;
 
+import com.greenfoxacademy.todomysql.model.ToDo;
+import com.greenfoxacademy.todomysql.repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodomysqlApplication {
+public class TodomysqlApplication implements CommandLineRunner {
+
+  @Autowired
+  ToDoRepository toDoRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(TodomysqlApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    toDoRepository.save(new ToDo("Learn MySQL"));
+    toDoRepository.save(new ToDo("What is JPA???"));
   }
 }
