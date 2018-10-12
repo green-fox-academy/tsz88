b26a76238b223b3e904f4f326ca39916f2088f08
Szilvia Toth <tothszilvia88@gmail.com>
2018-10-12 16:34:37 +0200

    added selenium jars and added webdriver inside the method

--- week-10/day-05/todoapp03/src/test/resources/TodoStepDefs.java
+++ week-10/day-05/todoapp03/src/test/resources/TodoStepDefs.java
@@ -1,15 +1,22 @@
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TodoStepDefs {
+  WebDriver driver;
+
   @When("^user is on main site$")
   public void userIsOnMainSite() throws Throwable {
-    System.out.println("user got to main site");
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\Acer\\Desktop\\WebDriver\\chromedriver.exe");
+    driver = new ChromeDriver();
+    driver.get("http://todomvc.com/examples/vanillajs/");
   }
 
   @Then("^user is able to see main site$")
   public void userIsAbleToSeeMainSite() throws Throwable {
-    System.out.println("user can see main site");
+    driver.findElement(By.className("new-todo"));
   }
 }
