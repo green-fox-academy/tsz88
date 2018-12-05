4036088a7823c8c7e55bac21091f89ae84b6a63b
Szilvia Toth <tothszilvia88@gmail.com>
2018-12-05 16:19:56 +0100

    feat: unit tests for fight and refill methods added

--- last-week/src/com/greenfoxacademy/AircraftTest.java
+++ last-week/src/com/greenfoxacademy/AircraftTest.java
@@ -1,15 +1,54 @@
 package com.greenfoxacademy;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AircraftTest {
 
   @Test
-  public void fight() {
+  public void emptyJetTryingToFight() {
+    JetF35 jet = new JetF35();
+    int actual = jet.fight();
+    Assert.assertEquals(0, actual);
   }
 
   @Test
-  public void refill() {
+  public void testingDamageCalculationPrecisionWithF35(){
+    JetF35 jet = new JetF35();
+    jet.refill(10);
+    int actualDamage = jet.fight();
+    Assert.assertEquals(500, actualDamage);
+  }
+
+  @Test
+  public void testDamageCalculationPrecisionWithF16(){
+    JetF16 jet = new JetF16();
+    jet.refill(10);
+    int actualDamage = jet.fight();
+    Assert.assertEquals(300, actualDamage);
+  }
+
+  @Test
+  public void refillNormally() {
+    JetF16 jet = new JetF16();
+    int actual = jet.refill(1000);
+    Assert.assertEquals(992, actual);
+  }
+
+  @Test
+  public void refillScarceResources(){
+    JetF35 jet = new JetF35();
+    int actual = jet.refill(11);
+    Assert.assertTrue(jet.currentAmmo == 11);
+    Assert.assertTrue(actual == 0);
+  }
+
+  @Test
+  public void refillWithZero(){
+    JetF35 jet = new JetF35();
+    int actual = jet.refill(0);
+    Assert.assertTrue(jet.currentAmmo == 0);
+    Assert.assertTrue(actual == 0);
   }
 
   @Test
