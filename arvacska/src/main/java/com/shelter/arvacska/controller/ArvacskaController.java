147852b33219faa827bcf946408756f616c41e52
Szilvia Toth <tothszilvia88@gmail.com>
2019-01-13 16:20:57 +0100

    init: started practice web app arvacska, based on shelter app

---
+++ arvacska/src/main/java/com/shelter/arvacska/controller/ArvacskaController.java
@@ -0,0 +1,15 @@
+package com.shelter.arvacska.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ArvacskaController {
+
+  @GetMapping("/")
+  public String mainPageDisplay(Model model){
+    return "main";
+  }
+}
---
+++ arvacska/src/test/java/com/shelter/arvacska/ArvacskaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shelter.arvacska;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArvacskaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
+
